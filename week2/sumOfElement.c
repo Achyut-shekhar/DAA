3d7181062e46bfcff99c339293e5b178b911a0d3
Achyut-shekhar <achyutshekhar54@gmail.com>
2025-02-15 23:15:46 +0530

    assignment

---
+++ week2/sumOfElement.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void findSequence(int arr[], int n)
+{
+  for (int k = 2; k < n; k++)
+  {
+    int i = 0, j = k - 1;
+    while (i < j)
+    {
+      int sum = arr[i] + arr[j];
+      if (sum == arr[k])
+      {
+        printf("%d %d %d\n", i, j, k);
+        return;
+      }
+      else if (sum < arr[k])
+      {
+        i++; // Increase sum
+      }
+      else
+      {
+        j--;
+      }
+    }
+  }
+  printf("No sequence found\n");
+}
+
+int main()
+{
+  int T;
+  printf("input the number of cases\n");
+  scanf("%d", &T);
+
+  while (T--)
+  {
+    int n;
+    printf("input the size of array\n");
+    scanf("%d", &n);
+    int arr[n];
+    printf("input the element of array size");
+    for (int i = 0; i < n; i++)
+    {
+      scanf("%d", &arr[i]);
+    }
+
+    findSequence(arr, n);
+  }
+
+  return 0;
+}
